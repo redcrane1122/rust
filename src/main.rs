0e4e60f1034bf712e43939bf973bfa2542e65366
redcrane1122 <redcrane1122@gmail.com>
2025-10-22 17:40:55 +0900

    updated for course

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+fn add_fn (a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn maybe_num() -> Option<i32> {
+    let num = 1;
+    match num {
+        1 => Some(num),
+        _ => None,
+    }
+}
+
+fn maybe_word() -> Option<String> {
+    return Some("A");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let add = |a: i32, b: i32| -> i32 {
+        a + b
+    };
+    let add = |a, b| a + b;
+    let sum = add (1,1);
+
+    let plus_one = match maybe_num() {
+        Some(num) => Some(num + 1),
+        None => None,
+    };
+
+    let plus_one = maybe_num().map(|num| num + 1);
+
+    let word_length = maybe_word()
+        .map(|word| word.len())
+        .map(|len| len * 2);
 }

6e6909b8fbda688b8086a1196f0779bddd5ebdfd
redcrane1122 <redcrane1122@gmail.com>
2025-11-06 15:46:33 +0900

    updated for error

---
+++ src/custom_error.rs
@@ -0,0 +1,37 @@
+#[derive(Debug, Error)]
+enum PassError {
+    #[error("Expired pass")]
+    PassExpired,
+    #[error("Insufficient funds: {0}")]
+    InsufficientFunds(isize),
+    #[error("Pass read error: {0}")]
+    ReadError(String),
+}
+
+fn swipe_card() -> Result<SubwayPass, PassError> {
+    Ok(SubwayPass {
+        id: 0,
+        funds: 200,
+        expires: Utc::now() + Duration::weeks(52),
+    })
+}
+
+fn use_pass(pass: &mut SubwayPass, cost: isize) -> Result<(), PassError> {
+    if Utc::now() > pass.expires {
+        Err(PassError::PassExpired)
+    } else {
+        if pass.fund - cost < 0 {
+            Err(PassError::InsufficientFunds(pass.funds))
+        } else {
+            pass.funds = pass.funds - cost;
+            Ok(())
+        }
+    }
+}
+
+fn main() {
+    let pass_status = swipe_card().and_then(|mut pass| use_pass(&mut pass, 3));
+    match pass_status {
+        Ok(_) => println!("ok to board")
+    }
+}
